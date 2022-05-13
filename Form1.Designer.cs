ab8e2f11001f8b6db7c063a29508f647925dc9ea
Vinh Tran <vinhquoc2103@gmail.com>
2022-05-13 18:20:08 +0700

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                int a;
             }
             base.Dispose(disposing);
         }
