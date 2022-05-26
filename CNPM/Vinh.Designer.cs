ef91d3da21665dd757cd27f5d86c748340aa1544
Vinh Tran <vinhquoc2103@gmail.com>
2022-05-26 19:00:35 +0700

    Thay đổi tên Form.cs thành Vinh.cs

--- CNPM/Vinh.Designer.cs
+++ CNPM/Vinh.Designer.cs
@@ -16,8 +16,6 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                int a;
-                int b;
             }
             base.Dispose(disposing);
         }
