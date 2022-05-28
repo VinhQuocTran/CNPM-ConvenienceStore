e04c0e7e4909d32dba8e21b1984fca152c4dc019
TranVanThao <vanthao000009@gmail.com>
2022-05-28 10:25:14 +0700

    Thảo đã tạo branch ManageProduct

--- CNPM/Vinh.cs
+++ CNPM/Vinh.cs
@@ -29,7 +29,7 @@ namespace WindowsFormsApp1
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
         }
     }
 }
