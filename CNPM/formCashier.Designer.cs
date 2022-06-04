1313f89467319ab65e5751b60448b042e860081d
Tran Minh Tri <tranminhtri0253@gmail.com>
2022-06-04 13:15:46 +0700

    Sửa lỗi chính tả + thêm formProgess

--- CNPM/formCashier.Designer.cs
+++ CNPM/formCashier.Designer.cs
@@ -38,7 +38,6 @@
             this.buttonCreateBill = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -48,34 +47,38 @@
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.Controls.Add(this.searchProductUserControl1);
             this.panel1.Controls.Add(this.createBillUserControl1);
-            this.panel1.Location = new System.Drawing.Point(188, 36);
+            this.panel1.Location = new System.Drawing.Point(141, 29);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(797, 673);
+            this.panel1.Size = new System.Drawing.Size(598, 547);
             this.panel1.TabIndex = 1;
             // 
             // searchProductUserControl1
             // 
             this.searchProductUserControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.searchProductUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.searchProductUserControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.searchProductUserControl1.Name = "searchProductUserControl1";
-            this.searchProductUserControl1.Size = new System.Drawing.Size(797, 673);
+            this.searchProductUserControl1.Size = new System.Drawing.Size(598, 547);
             this.searchProductUserControl1.TabIndex = 1;
             // 
             // createBillUserControl1
             // 
             this.createBillUserControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.createBillUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.createBillUserControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.createBillUserControl1.Name = "createBillUserControl1";
-            this.createBillUserControl1.Size = new System.Drawing.Size(797, 673);
+            this.createBillUserControl1.Size = new System.Drawing.Size(598, 547);
             this.createBillUserControl1.TabIndex = 0;
             // 
             // buttonLogout
             // 
             this.buttonLogout.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonLogout.Location = new System.Drawing.Point(12, 649);
+            this.buttonLogout.Location = new System.Drawing.Point(9, 527);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(160, 60);
+            this.buttonLogout.Size = new System.Drawing.Size(120, 49);
             this.buttonLogout.TabIndex = 5;
             this.buttonLogout.Text = "Đăng xuất";
             this.buttonLogout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -85,9 +88,10 @@
             // 
             this.buttonChangePassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonChangePassword.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonChangePassword.Location = new System.Drawing.Point(12, 431);
+            this.buttonChangePassword.Location = new System.Drawing.Point(9, 350);
+            this.buttonChangePassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonChangePassword.Name = "buttonChangePassword";
-            this.buttonChangePassword.Size = new System.Drawing.Size(160, 60);
+            this.buttonChangePassword.Size = new System.Drawing.Size(120, 49);
             this.buttonChangePassword.TabIndex = 4;
             this.buttonChangePassword.Text = "Đổi mật khẩu";
             this.buttonChangePassword.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -97,9 +101,10 @@
             // 
             this.buttonClearBill.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClearBill.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonClearBill.Location = new System.Drawing.Point(12, 331);
+            this.buttonClearBill.Location = new System.Drawing.Point(9, 269);
+            this.buttonClearBill.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonClearBill.Name = "buttonClearBill";
-            this.buttonClearBill.Size = new System.Drawing.Size(160, 60);
+            this.buttonClearBill.Size = new System.Drawing.Size(120, 49);
             this.buttonClearBill.TabIndex = 3;
             this.buttonClearBill.Text = "Xoá giỏ hàng";
             this.buttonClearBill.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -110,9 +115,10 @@
             // 
             this.buttonSearchProduct.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSearchProduct.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonSearchProduct.Location = new System.Drawing.Point(12, 238);
+            this.buttonSearchProduct.Location = new System.Drawing.Point(9, 193);
+            this.buttonSearchProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSearchProduct.Name = "buttonSearchProduct";
-            this.buttonSearchProduct.Size = new System.Drawing.Size(160, 60);
+            this.buttonSearchProduct.Size = new System.Drawing.Size(120, 49);
             this.buttonSearchProduct.TabIndex = 2;
             this.buttonSearchProduct.Text = "Sản phẩm";
             this.buttonSearchProduct.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -123,9 +129,10 @@
             // 
             this.buttonCreateBill.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCreateBill.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonCreateBill.Location = new System.Drawing.Point(12, 140);
+            this.buttonCreateBill.Location = new System.Drawing.Point(9, 114);
+            this.buttonCreateBill.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCreateBill.Name = "buttonCreateBill";
-            this.buttonCreateBill.Size = new System.Drawing.Size(160, 60);
+            this.buttonCreateBill.Size = new System.Drawing.Size(120, 49);
             this.buttonCreateBill.TabIndex = 0;
             this.buttonCreateBill.Text = "Giỏ hàng";
             this.buttonCreateBill.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -136,36 +143,28 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(42, 65);
+            this.label1.Location = new System.Drawing.Point(32, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Thu ngân";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(64, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(48, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(38, 41);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(97, 562);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // formCashier
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGreen;
-            this.ClientSize = new System.Drawing.Size(1006, 753);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(754, 612);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonLogout);
@@ -174,6 +173,7 @@
             this.Controls.Add(this.buttonSearchProduct);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonCreateBill);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "formCashier";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -199,7 +199,6 @@
         private ControlCreateBill createBillUserControl1;
         //public searchProductUserControl searchProductUserControl1;
         private ControlSearchProduct searchProductUserControl1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
